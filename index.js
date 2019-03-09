74ed433b7a01d8636aff4a46323bb1955023174c
Philip Smalls <philip.smalls86@gmail.com>
2019-03-09 01:30:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall (dwarves) {
+  let newArray = [];
+    for (let i = 0; i < dwarves.length; i++) {
+      newArray.push(`${i + 1}. ${dwarves[i]} ` );
+ }
+ return newArray.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+   let newArray = [];
+    for (let i = 0; i < planeteerCalls.length; i++) {
+      newArray.push(planeteerCalls[i].toString().toUpperCase() + '!') ;
+    }
+    return newArray;
 }
 
 function longPlaneteerCalls(words) {
+  if (words.length > 4) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function findTheCheese (foods) {
+if (foods.includes('cheddar' || 'gouda' || 'camebert' )) {
+  return 'cheddar' || 'gouda' || 'camebert' ;
+} else {
+  return 'no cheese!';
+ }
 }
+
+
+
