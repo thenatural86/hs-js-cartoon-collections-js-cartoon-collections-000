1a07c7494b9d70f9c31a8e556533b40c511d941c
Philip Smalls <philip.smalls86@gmail.com>
2019-04-25 00:43:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 function dwarfRollCall (dwarves) {
+  //initialze a new variable and set to empty array.
   let newArray = [];
+ // iterat through array and push expression interpolation of i+1 and element at current index. 
     for (let i = 0; i < dwarves.length; i++) {
       newArray.push(`${i + 1}. ${dwarves[i]} ` );
  }
+ //use .join() method on newArray to join and return a string.
  console.log(newArray.join(""));
  return newArray.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  //initialze new variable and set to empty array.
    let newArray = [];
+   //iterate through array and push element at current index to newArray and apply toUpperCase() 
+   //method. Concatenate '!'.
     for (let i = 0; i < planeteerCalls.length; i++) {
       newArray.push(planeteerCalls[i].toUpperCase() + '!') ;
     }
+    // return newArray
     console.log(newArray);
     return newArray;
 }
 
 function longPlaneteerCalls(words) {
+  //iterate through array
   for(let i = 0; i < words.length; i++){
+    //if current element is greater than 4 return true.
     if (words[i].length > 4) {
       console.log(words);
       return true;
     } 
+    // otherwise return false.
     return false;
   }
 }
 function findTheCheese (foods) {
+  //iterate through array, if currrent element is cheese return element
   for(let i = 0; i < foods.length; i++) {
     if (foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camebert' ) {
+      console.log(foods);
       return foods[i];
     } 
   }
+  // return message if array has no cheese.
     return 'no cheese!';
 }
 
