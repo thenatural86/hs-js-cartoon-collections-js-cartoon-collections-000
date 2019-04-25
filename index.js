2380d02758eb76db9b3333f960544217ea7a8bc3
Philip Smalls <philip.smalls86@gmail.com>
2019-04-25 18:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ function findTheCheese (foods) {
 function wordsThatStartWithB(words){
   for(let i = 0; i < words.length; i++) {
     if(words[i][0] === 'b') {
-    }
        return words[i];
+    }
   }
 }
 
